1b6abe3c762f19a01ec7ac94f02bb25933ba922b
Anthony Chu <antchu@microsoft.com>
2017-12-31 23:05:30 -0800

    Print go version

--- server.go
+++ server.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"runtime"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1><img src=\"https://raw.githubusercontent.com/ashleymcnamara/gophers/master/Azure_Gophers.png\" width=810 height=600 />")
+	fmt.Fprintf(w, "<h1>Hello from Go "+runtime.Version()+"!</h1><img src=\"https://raw.githubusercontent.com/ashleymcnamara/gophers/master/Azure_Gophers.png\" width=810 height=600 />")
 }
 func main() {
 	http.HandleFunc("/", handler)
