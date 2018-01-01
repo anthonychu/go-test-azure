32ba21148491aeff642cb941bf9dbf10f9ef6e42
Anthony Chu <antchu@microsoft.com>
2017-12-31 22:34:03 -0800

    Add custom deployment

--- server.go
+++ server.go
@@ -1,13 +1,15 @@
 package main
+
 import (
-    "fmt"
-    "net/http"
-    "os" 
+	"fmt"
+	"net/http"
+	"os"
 )
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there - this page was served using Go \\o/")
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1><img src=\"https://raw.githubusercontent.com/ashleymcnamara/gophers/master/Azure_Gophers.png\" width=810 height=600 />")
 }
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":"+os.Getenv("HTTP_PLATFORM_PORT"), nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+os.Getenv("HTTP_PLATFORM_PORT"), nil)
 }
